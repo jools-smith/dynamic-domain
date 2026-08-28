c32bb115f1ea1355eedee86b5e90c0debae2b77b
jools-smith <33135404+jools-smith@users.noreply.github.com>
2026-08-28 10:44:55 +0100

    added PostConstruct PreDestroy Scheduled

--- src/main/kotlin/revenera/gcs/dmdemo/DmDemoApplication.kt
+++ src/main/kotlin/revenera/gcs/dmdemo/DmDemoApplication.kt
@@ -1,10 +1,31 @@
-    package revenera.gcs.dmdemo
+package revenera.gcs.dmdemo
 
+import jakarta.annotation.PostConstruct
+import jakarta.annotation.PreDestroy
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
 
 @SpringBootApplication
-class DmDemoApplication
+@EnableScheduling
+class DmDemoApplication {
+
+    @PostConstruct
+    fun init() {
+        println("Bean initialized")
+    }
+
+    @PreDestroy
+    fun cleanup() {
+        println("Bean being destroyed")
+    }
+
+    @Scheduled(cron = "0 * * * * *")
+    fun runEveryMinute(){
+        println("Running every minute")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<DmDemoApplication>(*args)
